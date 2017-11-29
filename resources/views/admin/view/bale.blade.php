678a4ddfbfd894a558fdd1a7bdef534bd90a5665
chenw <1026039774@qq.com>
2017-11-29 23:45:19 +0800

    fix blade

--- resources/views/admin/view/bale.blade.php
+++ resources/views/admin/view/bale.blade.php
@@ -1,195 +1,149 @@
-<div class="menu_list">
-    <div class="first">
-        <dl class="list_dl">
-            <dt class="list_dt" id="open">
-                <span class="_after"></span>
-            <p>选择器</p>
-            <i class="list_dt_icon"></i>
-            </dt>
-            <dd class="list_dd" style="display: block">
-                <ul>
-                    <li class="list_li">#id</li>
-                    <li class="list_li">element</li>
-                    <li class="list_li">.class</li>
-                    <li class="list_li">*</li>
-                </ul>
-            </dd>
-            <dt class="list_dt">
-                <span class="_after"></span>
-            <p>属性</p>
-            <i class="list_dt_icon"></i>
-            </dt>
-            <dd class="list_dd">
-                <ul>
-                    <li class="list_li">attr</li>
-                    <li class="list_li">removeAttr</li>
-                    <li class="list_li">prop</li>
-                    <li class="list_li">removeProp</li>
-                    <li class="list_li">addClass</li>
-                    <li class="list_li">removeClass</li>
-                </ul>
-            </dd>
-            <dt class="list_dt">
-                <span class="_after"></span>
-                <p>文档处理</p>
-                <i class="list_dt_icon"></i>
-            </dt>
-            <dd class="list_dd">
-                <ul>
-                    <li class="list_li">append</li>
-                    <li class="list_li">appendTo</li>
-                    <li class="list_li">prepend</li>
-                    <li class="list_li">prependTo</li>
-                    <li class="list_li">after</li>
-                    <li class="list_li">before</li>
-                </ul>
-            </dd>
-            <dt class="list_dt">
-                <span class="_after"></span>
-            <p>事件</p>
-            <i class="list_dt_icon"></i>
-            </dt>
-            <dd class="list_dd">
-                <ul>
-                    <li class="list_li">ready</li>
-                    <li class="list_li">on</li>
-                    <li class="list_li">off</li>
-                    <li class="list_li">bind</li>
-                    <li class="list_li">one</li>
-                    <li class="list_li">trigger</li>
-                    <li class="list_li">hover</li>
-                    <li class="list_li">click</li>
-                    <li class="list_li">focus</li>
-                </ul>
-            </dd>
-            <dt class="list_dt">
-                <span class="_after"></span>
-            <p>AJAX</p>
-            <i class="list_dt_icon"></i>
-            </dt>
-            <dd class="list_dd">
-                <ul>
-                    <li class="list_li">$.ajax</li>
-                    <li class="list_li">$.get</li>
-                    <li class="list_li">$.getJSON</li>
-                    <li class="list_li">$.getScript</li>
-                    <li class="list_li">$.post</li>
-                </ul>
-            </dd>
-            <dt class="list_dt">
-                <span class="_after"></span>
-            <p>效果</p>
-            <i class="list_dt_icon"></i>
-            </dt>
-            <dd class="list_dd">
-                <ul>
-                    <li class="list_li">show</li>
-                    <li class="list_li">hide</li>
-                    <li class="list_li">toggle</li>
-                    <li class="list_li">slideDown</li>
-                    <li class="list_li">slideUp</li>
-                    <li class="list_li">slideToggle</li>
-                    <li class="list_li">fadeIn</li>
-                    <li class="list_li">fadeOut</li>
-                    <li class="list_li">fadeTo</li>
-                    <li class="list_li">fadeToggle</li>
-                    <li class="list_li">stop</li>
-                </ul>
-            </dd>
-        </dl>
-    </div>
-
-    <div class="second">
-        <div></div>
-    </div>
-</div>
-
 <style>
+    /*
+            author :前端一枚  努力学习中 qq：815183231;
+    */
+
+    /*简单粗暴重置默认样式===============================*/
     * {
         margin: 0;
         padding: 0;
-        font-size: 12px;
     }
 
-    .list_dt {
-        background: #333;
-        color: white;
-        width: 200px;
-        padding: 0 40px 0 20px;
-        height: 34px;
-        line-height: 34px;
-        cursor: pointer;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-        position: relative;
-        border-bottom: 1px solid #464646;
+    img {
+        border: 0;
     }
 
-    .list_dt:hover {
-        background: #222;
-    }
-
-    .list_dt:hover ._after {
-        display: block;
-        width: 3px;
-        height: 100%;
-        position: absolute;
-        left: 0;
-        top: 0;
-        background: red;
-    }
-
-    #open {
-        background: #222;
-    }
-
-    #open ._after {
-        display: block;
-        width: 3px;
-        height: 100%;
-        position: absolute;
-        left: 0;
-        top: 0;
-        background: #4285F4;
-    }
-
-    .list_dt_icon {
-        position: absolute;
-        right: 10px;
-        top: 9px;
-        display: block;
-        width: 16px;
-        height: 16px;
-        background: url("/web/admin/img/off.png") no-repeat;
-    }
-
-    #open .list_dt_icon {
-        background: url("/web/admin/img/open.png") no-repeat;
-    }
-
-    .list_dd {
-        display: none;
-    }
-
-    .list_dd ul {
-        margin-bottom: 0;
-    }
-
-    .list_li {
-        background: #4e4e4e;
+    ul, li {
         list-style-type: none;
-        color: white;
-        width: 200px;
-        padding: 0 30px;
-        height: 34px;
-        line-height: 34px;
-        cursor: pointer;
-        border-bottom: 1px solid #6b6b6b;
     }
 
-    .list_li:hover {
-        background: red;
+    a {
+        color: #00007F;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #bd0a01;
+        text-decoration: underline;
+    }
+
+    .treebox {
+        width: 200px;
+        margin: 0 auto;
+        background-color: #1a6cb9;
+    }
+
+    .menu {
+        overflow: hidden;
+        border-color: #ddd;
+        border-style: solid;
+        border-width: 0 1px 1px;
+    }
+
+    /*第一层*/
+    .menu li.level1 > a {
+        display: block;
+        height: 45px;
+        line-height: 45px;
+        color: #fff;
+        padding-left: 50px;
+        border-bottom: 1px solid #1a6cb9;
+        font-size: 16px;
+        position: relative;
+        transition: all .5s ease 0s;
+    }
+
+    .menu li.level1 a:hover {
+        text-decoration: none;
+        background-color: #326ea5;
+    }
+
+    .menu li.level1 a.current {
+        background: #0f4679;
+    }
+
+    /*============修饰图标*/
+    .ico {
+        width: 20px;
+        height: 20px;
+        display: block;
+        position: absolute;
+        left: 20px;
+        top: 10px;
+        background-repeat: no-repeat;
+        background-image: url(/web/admin/img/ico1.png);
+    }
+
+    /*============小箭头*/
+    .level1 i {
+        width: 20px;
+        height: 10px;
+        background-image: url(/web/admin/img/arrow.png);
+        background-repeat: no-repeat;
+        display: block;
+        position: absolute;
+        right: 20px;
+        top: 20px;
+    }
+
+    .level1 li div p.trilateral {
+        width: 0;
+        height: 0;
+        border-top: 6px solid transparent;
+        border-left: 6px solid #cccccc;
+        border-bottom: 6px solid transparent;
+        margin: 15px 10px;
+    }
+
+    .level2 div {
+        height: 45px;
+        float: right;
+    }
+
+    .level1 i.down {
+        background-position: 0 -10px;
+    }
+
+    .ico1 {
+        background-position: 0 0;
+    }
+
+    .ico2 {
+        background-position: 0 -20px;
+    }
+
+    .ico3 {
+        background-position: 0 -40px;
+    }
+
+    .ico4 {
+        background-position: 0 -60px;
+    }
+
+    .ico5 {
+        background-position: 0 -80px;
+    }
+
+    /*第二层*/
+    .menu li ul {
+        overflow: hidden;
+    }
+
+    .menu li ul.level2 {
+        display: none;
+        background: #0f4679;
+    }
+
+    .menu li ul.level2 li a {
+        display: block;
+        width: auto;
+        height: 45px;
+        line-height: 45px;
+        color: #fff;
+        text-indent: 20px;
+        /*border-bottom: 1px solid #ddd; */
+        font-size: 14px;
+        transition: all 1s ease 0s;
     }
 
     .first {
@@ -205,17 +159,80 @@
         height: 400px;
         background-color: #00a7d0;
         float: left;
+        margin-left: 10px;
     }
-</style>
 
-<script type="text/javascript">
-    $(".list_dt").on("click", function () {
-        $('.list_dd').stop();
-        $(this).siblings("dt").removeAttr("id");
-        if ($(this).attr("id") == "open") {
-            $(this).removeAttr("id").siblings("dd").slideUp();
-        } else {
-            $(this).attr("id", "open").next().slideDown().siblings("dd").slideUp();
-        }
-    });
+</style>
+<div class="first">
+    <div class="treebox">
+        <ul class="menu">
+            <li class="level1">
+                <a href="#none" class="current"><em class="ico ico1"></em>导航一<i class="down"></i></a>
+                <ul class="level2" style="display: block">
+                    <li>
+                        <a href="javascript:;">导航选项
+                            <div><p class="trilateral"></p></div>
+                        </a>
+
+                    </li>
+                    <li><a href="javascript:;">导航选项<div><p class="trilateral"></p></div></a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                </ul>
+            </li>
+            <li class="level1">
+                <a href="#none"><em class="ico ico2"></em>导航一<i></i></a>
+                <ul class="level2">
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                </ul>
+            </li>
+            <li class="level1">
+                <a href="#none"><em class="ico ico3"></em>导航一<i></i></a>
+                <ul class="level2">
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                </ul>
+            </li>
+            <li class="level1">
+                <a href="#none"><em class="ico ico4"></em>导航一<i></i></a>
+                <ul class="level2">
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                </ul>
+            </li>
+            <li class="level1">
+                <a href="#none"><em class="ico ico5"></em>导航一<i></i></a>
+                <ul class="level2">
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                    <li><a href="javascript:;">导航选项</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="second">
+
+</div>
+<script src="{{ asset('/web/admin/plug/easing.js') }}"></script>
+<script>
+    //等待dom元素加载完毕.
+    $(function () {
+        $(".treebox .level1>a").click(function () {
+            $(this).addClass('current')   //给当前元素添加"current"样式
+                .find('i').addClass('down')   //小箭头向下样式
+                .parent().next().slideDown('slow', 'easeOutQuad')  //下一个元素显示
+                .parent().siblings().children('a').removeClass('current')//父元素的兄弟元素的子元素去除"current"样式
+                .find('i').removeClass('down').parent().next().slideUp('slow', 'easeOutQuad');//隐藏
+            return false; //阻止默认时间
+        });
+    })
 </script>
