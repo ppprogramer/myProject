d6641c34441b5b90425630120f21cb2db75e232a
chenw <1026039774@qq.com>
2018-01-17 20:11:09 +0800

    sdd

--- app/Console/Commands/WeChat/Menu.php
+++ app/Console/Commands/WeChat/Menu.php
@@ -11,7 +11,7 @@ class Menu extends Command
      *
      * @var string
      */
-    protected $signature = 'wechat_menu';
+    protected $signature = 'wechat_menu{action}';
 
     /**
      * The console command description.
@@ -37,34 +37,39 @@ class Menu extends Command
      */
     public function handle()
     {
-        $buttons = [
-            [
-                "type" => "click",
-                "name" => "今日歌曲",
-                "key"  => "V1001_TODAY_MUSIC"
-            ],
-            [
-                "name"       => "菜单",
-                "sub_button" => [
-                    [
-                        "type" => "view",
-                        "name" => "搜索",
-                        "url"  => "http://www.soso.com/"
-                    ],
-                    [
-                        "type" => "view",
-                        "name" => "视频",
-                        "url"  => "http://v.qq.com/"
-                    ],
-                    [
-                        "type" => "click",
-                        "name" => "赞一下我们",
-                        "key" => "V1001_GOOD"
+        $action = $this->argument('action');
+        $app = app('wechat.official_account');
+        if ($action == 'create') {
+            $buttons = [
+                [
+                    "type" => "click",
+                    "name" => "今日歌曲",
+                    "key" => "V1001_TODAY_MUSIC"
+                ],
+                [
+                    "name" => "菜单",
+                    "sub_button" => [
+                        [
+                            "type" => "view",
+                            "name" => "搜索",
+                            "url" => "http://www.soso.com/"
+                        ],
+                        [
+                            "type" => "view",
+                            "name" => "视频",
+                            "url" => "http://v.qq.com/"
+                        ],
+                        [
+                            "type" => "click",
+                            "name" => "赞一下我们",
+                            "key" => "V1001_GOOD"
+                        ],
                     ],
                 ],
-            ],
-        ];
-        $app = app('wechat.official_account');
-        $app->menu->create($buttons);
+            ];
+            $app->menu->create($buttons);
+        } else if ($action == 'delete') {
+            $app->menu->delete();
+        }
     }
 }
