d7c3e72761dc0585e05b75a2eb8df03efa390782
chenw <1026039774@qq.com>
2018-01-17 19:59:49 +0800

    vlid

--- app/Http/Controllers/WeChat/WeChatController.php
+++ app/Http/Controllers/WeChat/WeChatController.php
@@ -11,37 +11,43 @@ class WeChatController extends Controller
 
     public function serve()
     {
-        $app = app('wechat.official_account');
-        $app->server->push(function($message){
-            switch ($message['MsgType']) {
-                case 'event':
-                    return '收到事件消息';
-                    break;
-                case 'text':
-                    return '收到文字消息';
-                    break;
-                case 'image':
-                    return '收到图片消息';
-                    break;
-                case 'voice':
-                    return '收到语音消息';
-                    break;
-                case 'video':
-                    return '收到视频消息';
-                    break;
-                case 'location':
-                    return '收到坐标消息';
-                    break;
-                case 'link':
-                    return '收到链接消息';
-                    break;
-                // ... 其它消息
-                default:
-                    return "欢迎关注 overtrue！";
-                    break;
-            }
-        });
-        return $app->server->serve();
+        header('Content-type:text');
+        if (isset($_GET['echostr'])) {
+            $this->valid();
+        }else{
+            $this->responseMsg();
+        }
+//        $app = app('wechat.official_account');
+//        $app->server->push(function($message){
+//            switch ($message['MsgType']) {
+//                case 'event':
+//                    return '收到事件消息';
+//                    break;
+//                case 'text':
+//                    return '收到文字消息';
+//                    break;
+//                case 'image':
+//                    return '收到图片消息';
+//                    break;
+//                case 'voice':
+//                    return '收到语音消息';
+//                    break;
+//                case 'video':
+//                    return '收到视频消息';
+//                    break;
+//                case 'location':
+//                    return '收到坐标消息';
+//                    break;
+//                case 'link':
+//                    return '收到链接消息';
+//                    break;
+//                // ... 其它消息
+//                default:
+//                    return "欢迎关注 overtrue！";
+//                    break;
+//            }
+//        });
+//        return $app->server->serve();
     }
 
     public function valid()
