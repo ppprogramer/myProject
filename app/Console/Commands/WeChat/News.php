eca974635abc451a22513d6abf7e34222e9c71e8
chenwei <1026039774@qq.com>
2018-01-19 16:45:47 +0800

    dd

--- app/Console/Commands/WeChat/News.php
+++ app/Console/Commands/WeChat/News.php
@@ -42,11 +42,11 @@ class News extends Command
         $app = app('wechat.official_account');
         $materialImageText = WeChatMaterialImageText::orderBy('id', 'desc')->first();
 //        $app->broadcasting->sendNews($materialImageText->media_id);
-        $app->broadcasting->sendText("大家好！欢迎使用 EasyWeChat。");
-//        $weChatUser = WeChatUsers::where('subscribe', 1)->get();
-//        foreach ($weChatUser as $item) {
-//            if ($materialImageText)
-//                $app->broadcasting->previewNews($materialImageText->media_id, $item->openid);
-//        }
+//        $app->broadcasting->sendText("大家好！欢迎使用 EasyWeChat。");
+        $weChatUser = WeChatUsers::where('subscribe', 1)->get();
+        foreach ($weChatUser as $item) {
+            if ($materialImageText)
+                $app->broadcasting->previewNews($materialImageText->media_id, $item->openid);
+        }
     }
 }
