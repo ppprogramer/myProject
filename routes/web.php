a08f331ea0cd6e25349a05e2114d441c3ef669de
chenw <1026039774@qq.com>
2018-01-28 10:57:43 +0800

    dd

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ Route::group(['middleware' => 'web'], function () {
     //微信
     Route::any('/wechat', 'WeChat\WeChatController@server')->name('wechat.server');
     //小程序
-    Route::group(['prefix' => 'wechatMini'], function () {
-        Route::get('/auth/cookie', 'WeChatMini\WeChatMiniController@cookie')->name('wechatMini.auth.cookie');
-        Route::post('/auth/login', 'WeChatMini\WeChatMiniController@login')->name('wechatMini.auth.login');
+    Route::group(['namespace' => 'WeChatMini', 'prefix' => 'wechatMini'], function () {
+        Route::get('/auth/cookie', 'WeChatMiniAuthController@cookie')->name('wechatMini.auth.cookie');
+        Route::post('/auth/login', 'WeChatMiniAuthController@login')->name('wechatMini.auth.login');
 
         Route::group(['middleware' => 'wx.mini.auth'], function () {
-            Route::post('/banner/list', 'WeChatMini\WeChatMiniBannerController@bannerList')->name('wechatMini.banner.list');
+            Route::post('/banner/list', 'WeChatMiniBannerController@bannerList')->name('wechatMini.banner.list');
         });
     });
 
