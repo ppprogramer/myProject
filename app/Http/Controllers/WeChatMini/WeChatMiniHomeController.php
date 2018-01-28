04c0324ce2dfa328e9df60cb49752a363df61e14
chenw <1026039774@qq.com>
2018-01-28 12:00:54 +0800

    categories

--- app/Http/Controllers/WeChatMini/WeChatMiniHomeController.php
+++ app/Http/Controllers/WeChatMini/WeChatMiniHomeController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers\WeChatMini;
 
 use App\Http\Controllers\Controller;
+use App\Models\Item;
 use App\Models\RollPicture;
 
-class WeChatMiniBannerController extends Controller
+class WeChatMiniHomeController extends Controller
 {
-    public function bannerList()
+    public function banner()
     {
         $list = RollPicture::select(['name', 'id'])->orderBy('id', 'desc')->limit(5)->get();
         $domain = 'http://' . request()->server('HTTP_HOST');
         foreach ($list as $item) {
             $item->name = $domain . "/uploads/$item->name";
         }
+        return ['list' => $list, 'code' => 0, 'msg' => '获取成功'];
+    }
+
+    public function categories()
+    {
+        $cateId = 4;            //服饰鞋包
+        $list = Item::where('pid', $cateId)->get();
         return ['list' => $list, 'code' => 0, 'msg' => '获取成功'];
     }
 }
