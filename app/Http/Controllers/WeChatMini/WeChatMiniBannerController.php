e1ff9e716c1dda35baba9f3525a280684e28c1a4
chenw <1026039774@qq.com>
2018-01-28 11:22:59 +0800

    dd

--- app/Http/Controllers/WeChatMini/WeChatMiniBannerController.php
+++ app/Http/Controllers/WeChatMini/WeChatMiniBannerController.php
@@ -10,8 +10,9 @@ class WeChatMiniBannerController extends Controller
     public function bannerList()
     {
         $list = RollPicture::select(['name', 'id'])->orderBy('id', 'desc')->limit(5)->get();
+        $domain = 'http://' . request()->server('HTTP_HOST');
         foreach ($list as $item) {
-            $item->name = "/uploads/$item->name";
+            $item->name = $domain . "/uploads/$item->name";
         }
         return ['list' => $list, 'code' => 0, 'msg' => '获取成功'];
     }
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::group(['middleware' => 'web'], function () {
     Route::group(['namespace' => 'WeChatMini', 'prefix' => 'wechatMini'], function () {
         Route::get('/auth/cookie', 'WeChatMiniAuthController@cookie')->name('wechatMini.auth.cookie');
         Route::post('/auth/login', 'WeChatMiniAuthController@login')->name('wechatMini.auth.login');
-
+        Route::get('/banner/list', 'WeChatMiniBannerController@bannerList')->name('wechatMini.banner.list');
         Route::group(['middleware' => 'wx.mini.auth'], function () {
-            Route::post('/banner/list', 'WeChatMiniBannerController@bannerList')->name('wechatMini.banner.list');
+//            Route::get('/banner/list', 'WeChatMiniBannerController@bannerList')->name('wechatMini.banner.list');
         });
     });
 
